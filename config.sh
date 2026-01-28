6441e26a7d4f93fcc08298d33508b6f948d2dcf3
alessandro-stella <alessandro.stella2004@gmail.com>
2026-01-28 11:44:25 +0100

    Added config external variables

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+USERNAME="utente123"
+PASSWORD="segreto"
+INSTALL_DIR="/opt/mio_programma"
--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Esecuzione script..."
+echo "Starting install script..."
 
 # esempio: installazione di un pacchetto
 if ! command -v curl >/dev/null 2>&1; then
-  echo "curl non installato"
+  echo "curl non installed"
   exit 1
 fi
 
-echo "Tutto ok"
+source ./config.sh
+echo "User: $USERNAME"
+echo "Password: $PASSWORD"
+echo "Install dir: $INSTALL_DIR"
