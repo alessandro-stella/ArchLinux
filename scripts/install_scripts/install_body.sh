0cda267b5e1d408916a528a2daee4502f84eb87e
alessandro-stella <alessandro.stella2004@gmail.com>
2026-01-29 20:41:31 +0100

    Added sudo refresh and optimized packet installation

--- scripts/install_scripts/install_body.sh
+++ scripts/install_scripts/install_body.sh
@@ -18,6 +18,10 @@ USER_NAME="$SUDO_USER"
 HOME="/home/$USER_NAME"
 CONFIG="$HOME/.config"
 
+# Loop to keep sudo active
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo
 echo "Starting install script..."
 
@@ -100,17 +104,16 @@ if [[ "$confirm" != "y" ]]; then
 fi
 
 # Install pacman packages
-for pkg in "${PACMAN_PACKAGES[@]}"; do
-    echo "Installing $pkg..."
-    sudo pacman -S --noconfirm "$pkg"
-done
+if [ "${#PACMAN_PACKAGES[@]}" -gt 0 ]; then
+    echo "Installing all pacman packages..."
+    sudo pacman -S --noconfirm "${PACMAN_PACKAGES[@]}"
+fi
 
 # Install yay packages
-for pkg in "${YAY_PACKAGES[@]}"; do
-    echo "Installing $pkg..."
-    sudo -u "$USER_NAME" -H yay -S --noconfirm "$pkg"
-done
-
+if [ "${#YAY_PACKAGES[@]}" -gt 0 ]; then
+    echo "Installing all yay packages..."
+    sudo -u "$USER_NAME" -H yay -S --noconfirm "${YAY_PACKAGES[@]}"
+fi
 # Install external packages
 for pkg in "${!EXTERNAL_PACKAGES[@]}"; do
     url="${EXTERNAL_PACKAGES[$pkg]}"
@@ -242,6 +245,9 @@ ufw --force enable
 
 # Delete installation script
 sudo rm "$CONFIG/install.sh"
+
+# Clean sudo refresh added at the start
+kill $(jobs -p) 2>/dev/null || true
 
 echo
 echo "Installation completed!"
